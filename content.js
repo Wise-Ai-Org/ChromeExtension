73d42898a46fe5f406668c9d2b2b0c715fafa7b7
Kristina Tiskeviciute <kris@sellwisely.ai>
2023-09-20 20:12:29 -0700

    Update content.js
    stuuuffff

--- content.js
+++ content.js
@@ -1,87 +1,3 @@
-/*
-let script = [];
-last_span = '';
-let last_speaker = "";
-
-// Create a MutationObserver instance
-const transcriptObserver = new MutationObserver((mutations) => {
-  //chrome.runtime.sendMessage({ mutations });
-  
-  mutations.forEach((mutation) => {
-    if (mutation.target.classList && mutation.target.classList.contains("iTTPOb")){
-      if (mutation.addedNodes.length) {
-        var newNodes = mutation.addedNodes;
-        var speaker = newNodes["0"]?.parentNode?.parentNode?.parentNode?.querySelector(".zs7s8d.jxFHg")?.textContent;
-        console.log(newNodes["0"]);
-        //console.log(newNodes["0"]?.textContent);
-        if (speaker) {
-          setTimeout(function () {
-            if (newNodes.length) {
-              if (last_speaker != speaker) {
-                console.log("Speaker:" + speaker);
-                console.log(newNodes["0"]);
-              } else {
-              }
-            }
-          }, 10000);
-        }
-      }
-    }
-  });
-});
-
-const divObserver = new MutationObserver(() => {
-  var divT4LgNb = document.getElementsByClassName("T4LgNb")[0];
-  
-  if (divT4LgNb) {
-    var divkFwPee = divT4LgNb.getElementsByClassName("kFwPee")[0];
-    
-    if (divkFwPee) {
-      var divcrqnQb = divkFwPee.getElementsByClassName("crqnQb")[0];
-      
-      if (divcrqnQb) {
-        var diva4cQT = divcrqnQb.getElementsByClassName("a4cQT")[0];
-        
-        if (diva4cQT) {
-          firstSubElementa4CQT = diva4cQT.children[0].children[0];
-          
-          if (firstSubElementa4CQT) {
-            console.log("The div iOzk7 is present", firstSubElementa4CQT);
-            
-            const targetNode = firstSubElementa4CQT;
-
-            //const config = { childList: true, subtree: true, characterData: true};
-            const config = { childList: true, subtree: true, attributes: false, characterData: false,};
-
-            divObserver.disconnect();
-            console.log("The divObserver was disconnected");
-
-            transcriptObserver.observe(targetNode, config);
-
-          } else {
-            console.log("The div iOzk7 is not present");
-          }
-        } else {
-          console.log("The div a4cQT is not present. Meeting is not joined yet.");
-        }
-      
-      } else {
-        console.log("The div crqnQb is not present");
-      }
-    } else {
-      console.log("The div kFwPee is not present. It is the meet.google.com page. Not the meeting page");
-    }
-  } else {
-    console.log("The div T4LgNb is not present");
-  }
-});
-
-const targetNode = document.body;
-
-const config = { childList: true, subtree: true };
-
-divObserver.observe(targetNode, config);
-*/
 
 let script = [];
 let last_span = '';
@@ -90,12 +6,9 @@ let last_speaker = '';
 // Create a MutationObserver instance for transcript changes
 const transcriptObserver = new MutationObserver((mutations) => {
   mutations.forEach((mutation) => {
-    //chrome.runtime.sendMessage({ mutations });
-
     // Check if the mutation is related to the transcript
     if (mutation.target.classList && mutation.target.classList.contains('iTTPOb')) {
       if (mutation.addedNodes.length) {
-        
         const newNodes = mutation.addedNodes;
         const speaker = newNodes[0]?.parentNode?.parentNode?.parentNode?.querySelector('.zs7s8d.jxFHg')?.textContent;
 
@@ -104,14 +17,10 @@ const transcriptObserver = new MutationObserver((mutations) => {
             if (newNodes.length) {
               if (last_speaker !== speaker) {
                 script.push({'speaker':speaker, "script": newNodes["0"].innerText + "\r\n"});
-                //script.push(speaker + " : " + newNodes["0"].innerText + "\r\n");
                 last_speaker = speaker;
-                //console.log('Speaker:', speaker);
-                //console.log(newNodes[0]);
-                //console.log('Speaker:', speaker, "Text:", script);
+                console.log('Speaker:', speaker, "Text:", script);
               } else {
                 var lastText = script.pop();
-                //lastText = lastText.slice(0, -2);
                 lastText['script']  = lastText['script'] + newNodes["0"].innerText + "\r\n";
                 script.push(lastText);
               }
@@ -122,7 +31,7 @@ const transcriptObserver = new MutationObserver((mutations) => {
     }
 
     for (const oldNode of mutation.removedNodes) {
-      if (oldNode.classList.value==='TBMuR bj4p3b');{
+      if (oldNode?.classList?.value==='TBMuR bj4p3b');{
         lastConvo = script.shift();
         last_speaker = '';
         if (lastConvo){
@@ -136,53 +45,32 @@ const transcriptObserver = new MutationObserver((mutations) => {
 
 // Create a MutationObserver instance for detecting the meeting page
 const divObserver = new MutationObserver(() => {
-  const divT4LgNb = document.getElementsByClassName('T4LgNb')[0];
-  
-  if (divT4LgNb) {
-    const divkFwPee = divT4LgNb.getElementsByClassName('kFwPee')[0];
-    
-    if (divkFwPee) {
-      const divcrqnQb = divkFwPee.getElementsByClassName('crqnQb')[0];
-      
-      if (divcrqnQb) {
-        const diva4cQT = divcrqnQb.getElementsByClassName('a4cQT')[0];
-        
-        if (diva4cQT) {
-          const firstSubElementa4CQT = diva4cQT.children[0].children[0];
-          
-          if (firstSubElementa4CQT) {
-            console.log('The div iOzk7 is present', firstSubElementa4CQT);
-            
-            const targetNode = firstSubElementa4CQT;
-            const config = {
-              childList: true,
-              subtree: true,
-              attributes: false,
-              characterData: false,
-            };
-            
-            divObserver.disconnect();
-            console.log('The divObserver was disconnected');
-            
-            transcriptObserver.observe(targetNode, config);
-          } else {
-            console.log('The div iOzk7 is not present');
-          }
-        } else {
-          console.log('The div a4cQT is not present. Meeting is not joined yet.');
-        }
-      } else {
-        console.log('The div crqnQb is not present');
-      }
-    } else {
-      console.log('The div kFwPee is not present. It is the meet.google.com page. Not the meeting page');
-    }
+  const ccButtonXpath = '//button[@jsname="r8qRAd"]';
+  const result = document.evaluate(ccButtonXpath , document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+  const ccButton = result.singleNodeValue;
+
+  if (ccButton) {
+    ccButton.click();
+    console.log('ccButton found')
+    const captionDivXpath = '//div[@class="iOzk7"]';
+    const captionDivXpathResult = document.evaluate(captionDivXpath , document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+    const captionDiv = captionDivXpathResult.singleNodeValue;
+    const config = {
+      childList: true,
+      subtree: true,
+      attributes: false,
+      characterData: false,
+    };
+    divObserver.disconnect();
+    console.log('The divObserver was disconnected');
+    transcriptObserver.observe(captionDiv, config);
+    //uncomment to hide cc div
+    //targetNode.parentNode.parentNode.style.height = '1px'
   } else {
-    console.log('The div T4LgNb is not present');
+    console.log('ccButton not found');
   }
 });
 
 const targetNode = document.body;
 const config = { childList: true, subtree: true };
-
 divObserver.observe(targetNode, config);
