a649498473896862f0992f6f69aecdbef2e769e2
wiseabhisek <134566301+wiseabhisek@users.noreply.github.com>
2023-09-21 05:27:30 -0400

    Added time stamp and transcript handling

--- background.js
+++ background.js
@@ -1,6 +1,51 @@
+let dataBuffer = []; // Array to store received data for 30 seconds
+let toBeSentStack = []; // Queue to hold data waiting to be sent to the server
+
+// Function to send data to the server
+async function sendDataToServer(data) {
+    // Simulate a POST request to the server (replace with your actual code)
+    // You can use fetch or any other HTTP library for this purpose
+    console.log("Sending data to server:", data);
+    try {
+        const response = await fetch('https://inwise-node-functions.azurewebsites.net/api/dump_delta_transcript?code=FwBcCM4aXiYlOZVlXHb2p-GwuIg7X6pvIyPL3mCADA3NAzFu4ytNLA==', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        });
+        console.log("Server confirmed receipt of data:", response);} catch (error) {
+        console.error(error);
+    }
+
+    // Simulate a successful response for demonstration purposes
+    setTimeout(() => {
+        console.log("Server confirmed receipt of data:", data);
+        toBeSentStack.shift(); // Remove the oldest item from the queue
+        if (toBeSentStack.length > 0) {
+            // If there is pending data in the queue, send the next one immediately
+            sendDataToServer(toBeSentStack[0]);
+        }
+    }, 2000); // Simulating a 2-second delay for the server response
+}
+
 // Receive the mutation data sent from the content script
 chrome.runtime.onMessage.addListener((mutationData) => {
-    // Handle the received mutation data as needed
-    console.log("Received mutation data:", mutationData);
+    // Push the received data to the buffer array
+    dataBuffer.push(mutationData);
+});
 
-  });  
+// Function to queue data from dataBuffer to toBeSentStack after 30 seconds
+function queueDataAfterDelay() {
+    if (dataBuffer.length > 0) {
+        toBeSentStack.push(dataBuffer.slice()); // Queue a copy of dataBuffer
+        dataBuffer = []; // Clear dataBuffer
+        // Attempt to send the oldest item in the queue if not already sending
+        if (toBeSentStack.length === 1) {
+            sendDataToServer(toBeSentStack[0]);
+        }
+    }
+}
+
+// Set an interval to queue data every 30 seconds
+setInterval(queueDataAfterDelay, 30000);
--- content.js
+++ content.js
@@ -1,5 +1,7 @@
-
+// Initialize an array to store the transcript script
 let script = [];
+
+// Initialize variables to keep track of the last span and speaker
 let last_span = '';
 let last_speaker = '';
 
@@ -13,12 +15,12 @@ const transcriptObserver = new MutationObserver((mutations) => {
         const speaker = newNodes[0]?.parentNode?.parentNode?.parentNode?.querySelector('.zs7s8d.jxFHg')?.textContent;
 
         if (speaker) {
+          // Delay processing by 10 seconds to ensure full transcript is loaded
           setTimeout(function () {
             if (newNodes.length) {
               if (last_speaker !== speaker) {
                 script.push({'speaker':speaker, "script": newNodes["0"].innerText + "\r\n"});
                 last_speaker = speaker;
-                console.log('Speaker:', speaker, "Text:", script);
               } else {
                 var lastText = script.pop();
                 lastText['script']  = lastText['script'] + newNodes["0"].innerText + "\r\n";
@@ -30,13 +32,14 @@ const transcriptObserver = new MutationObserver((mutations) => {
       }
     }
 
+    // Check for removed nodes
     for (const oldNode of mutation.removedNodes) {
       if (oldNode?.classList?.value==='TBMuR bj4p3b');{
         lastConvo = script.shift();
         last_speaker = '';
         if (lastConvo){
-          console.log({ "URL":document.URL, lastConvo });
-          chrome.runtime.sendMessage({ "URL":document.URL, lastConvo });
+          console.log({ "URL":document.URL, 'Time' : new Date().getTime(), "Conversation": lastConvo });
+          chrome.runtime.sendMessage({ "URL":document.URL, 'Time' : new Date().getTime(), "Conversation": lastConvo });
         }
       } 
     }
@@ -45,25 +48,36 @@ const transcriptObserver = new MutationObserver((mutations) => {
 
 // Create a MutationObserver instance for detecting the meeting page
 const divObserver = new MutationObserver(() => {
+  // XPath to find the closed caption button
   const ccButtonXpath = '//button[@jsname="r8qRAd"]';
   const result = document.evaluate(ccButtonXpath , document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
   const ccButton = result.singleNodeValue;
 
   if (ccButton) {
+    // Click the closed caption button
     ccButton.click();
     console.log('ccButton found')
+
+    // XPath to find the caption div
     const captionDivXpath = '//div[@class="iOzk7"]';
     const captionDivXpathResult = document.evaluate(captionDivXpath , document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
     const captionDiv = captionDivXpathResult.singleNodeValue;
+
+    // Configure MutationObserver to observe caption changes
     const config = {
       childList: true,
       subtree: true,
       attributes: false,
       characterData: false,
     };
+
+    // Disconnect the divObserver since it's no longer needed
     divObserver.disconnect();
     console.log('The divObserver was disconnected');
+
+    // Start observing transcript changes
     transcriptObserver.observe(captionDiv, config);
+
     //uncomment to hide cc div
     //targetNode.parentNode.parentNode.style.height = '1px'
   } else {
@@ -71,6 +85,9 @@ const divObserver = new MutationObserver(() => {
   }
 });
 
+// Define the target node for divObserver
 const targetNode = document.body;
 const config = { childList: true, subtree: true };
+
+// Start observing changes in the target node
 divObserver.observe(targetNode, config);
